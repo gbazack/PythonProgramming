f4766bbcf7bdb2717dd478e2c42276c2c6750a10
Edinio Zacko <morenzack@gmail.com>
2021-01-16 21:15:10 +0100

    Update Tutorial1.2_Skulix.py

--- Tutorial1.2_Skulix.py
+++ Tutorial1.2_Skulix.py
@@ -2,7 +2,39 @@
 In this tutorial, you will learn to implement a school management system with Python classes and interact with it via functions
 @gbazack, Jan 2021
 """
+#Create a main class with common attributes and methods
+#Then we create subclasses for student, teacher and course that will inherit from the main class
+class Entity():
+  def __init__(self, ID, names):
+    self.ID= ID
+    self.names= names
+  
+  def getID(self):
+    return self.ID
+  def getName():
+    retunr self.names
+  
+  def setID(self, Id):
+    self.ID= Id
+  def setName(self, Name):
+    self.names= Name
 
+#Create a class Person to inherit from Entity
+class Person(Entity):
+  def __init__(self, age, sex):
+		self.age= age
+		self.sex= sex
+		
+	def getAge(self):
+    return self.age
+  def getSex():
+    retunr self.sex
+  
+  def setAge(self, Age):
+    self.age= Age
+  def setSex(self, Sex):
+    self.sex= Sex
+		
 #Create a database to save students
 student=dict(ID=[1, 2], names=["Joel Ndoumbe", "Paule Hadja"], age=[1,2], level=["Master", "Master"], sex=["Male", "Female"])
 #Create a database to save teacher.
