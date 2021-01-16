c20006fece630f65d71460ebe219b96dc98218a0
Edinio Zacko <morenzack@gmail.com>
2021-01-16 06:53:04 +0100

    Update tutorial1.py

--- tutorial1.py
+++ tutorial1.py
@@ -1,17 +1,6 @@
 """
-In this tutorial, you will learn how to implement a school management system with Python data structures such as list and dictionary and interact with it via functions. More precisely, you'll learn to
-1. Create databases:
-   1. A database to save students’ information. This information includes the student’s identification number, names, age, level, sex.
-   2. A database to save teacher’s (lecturer) information such as his/her ID, name, age, sex, course ID.
-   3. A database to register courses. Each course is described by an ID, title, category, lecturer ID.
-   4. A database to save the notes. Notes should have the following attributes: Note ID, student ID, Course ID, and values.
-2. Manipulate these databases:
-   1. Search items from
-   2. Add items to
-   3. Remove items from
-   4. Modify items of
-
-For more information, visit our YouTube channel via [https://www.youtube.com/channel](https://www.youtube.com/channel/UCBqqJWMUr74vAtl65ZWc20A/featured) and subscribe to get notified whenever there's a new video.
+In this tutorial, you will learn how to implement a school management system with Python data structures such as list and dictionary and interact with it via functions
+@gbazack, Jan 2021
 """
 
 #Create a database to save students
