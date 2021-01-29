6f74b866529e72362384acbee1da191866e1aa45
Edinio Zacko <morenzack@gmail.com>
2021-01-29 17:18:54 +0100

    Add files via upload

---
+++ mynumbers/prime/generator.py
@@ -0,0 +1,16 @@
+"""
+This module contains functions
+which generate prime numbers
+and test if an input number is prime
+"""
+
+def generate(n):
+   pnList=[2,3]
+   nlist=list(range(4,n+1))
+   
+   for x in nlist:
+      y=[math.gcd(i,x) for i in nlist]      
+      if(y.count(1)+y.count(x))==len(y):
+         pnList.append(x)
+   return pnList
+
